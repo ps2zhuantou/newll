477208f76cc68734f61dc1ae4fd76d0a495213c5
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2020-03-30 20:54:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,3 +9,4 @@
 # sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/pymumu/smartdns.git package/smartdns
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
