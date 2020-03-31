0613f9fd93a77d1861fd79987e2a00b7c291cee5
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2020-03-31 11:39:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,8 +7,8 @@
 #=================================================
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 git clone https://github.com/pymumu/smartdns.git package/smartdns
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 ./scripts/feeds update -a
 ./scripts/feeds install -a
