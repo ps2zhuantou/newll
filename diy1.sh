ae036e31283e05addd5eafe48e4f75d45e631002
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2020-04-01 11:41:30 +0800

    Create diy1.sh

---
+++ diy1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/Lienol/openwrt-package.git package/openwrt-package
+git clone https://github.com/ujincn/luci-app-smartdns-compat.git package/luci-app-smartdns-compat
+git clone https://github.com/pymumu/smartdns.git package/smartdns
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+./scripts/feeds update -a
+./scripts/feeds install -a
