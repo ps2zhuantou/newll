456d60bb22313bb2058bb20b02010184e1dfe3f5
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2021-08-18 13:36:57 +0800

    Update diy-immortalwrt.sh

--- diy-immortalwrt.sh
+++ diy-immortalwrt.sh
@@ -12,9 +12,9 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 # git clone https://github.com/kenzok8/small.git package/small
-git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+# git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon package/luci-theme-atmaterial-ColorIcon
-git clone https://github.com/abctel/luci-theme-edge package/luci-theme-edge
+# git clone https://github.com/abctel/luci-theme-edge package/luci-theme-edge
 # git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 # git clone https://github.com/kenzok8/litte.git package/litte
 ./scripts/feeds update -a
