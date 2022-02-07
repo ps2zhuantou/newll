06eb27c09e8fff97af376969f416363ddf0d88c0
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2022-02-07 10:33:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,7 @@ git clone https://github.com/kenzok8/small.git package/small
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon package/luci-theme-atmaterial-ColorIcon
 git clone https://github.com/fszok/luci-app-advancedsetting package/luci-app-advancedsetting
+git clone https://github.com/QiuSimons/openwrt-mos package/openwrt-mos
 # git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 # git clone https://github.com/kenzok8/litte.git package/litte
 ./scripts/feeds update -a
